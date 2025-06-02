e92fe8f6846f51773fbe2a9b126731b7e91dadf9
MD.Kamrul Hasan ⚡️ <kamrulhasan635653@gmail.com>
2025-06-02 23:57:51 +0600

    first commit

---
+++ bin/dart_pratice.dart
@@ -0,0 +1,59 @@
+void main() {
+
+  print("Bismillah");
+
+  var x = 10;
+  var y = 40;
+  var z = x + y;
+
+  print(z);
+
+
+  var myCountry = 'My Country Name is Bangadesh';
+
+  var isBangladeshi = true;
+
+  print(isBangladeshi);
+  
+  var city = ['Dhaka', 'Chattogram', 'Khulna', 'Barisal', 12, 14.4, true];
+  print(city);
+  print(city[4]);
+  print(city.length);
+
+  var stuednts = {
+    'name': "Kamrul",
+    'age': 27,
+    'Department': 'CSE',
+    'Branch': '20th'
+  };
+
+  print(stuednts['name']);
+
+  var a = 10;
+  var b = 20;
+  var c = 3;
+
+  print(a + b);
+  print(a - b);
+  print(a * b);
+  print(a / b);
+  print(a % c);
+
+  print("___________________");
+
+  print(++a);
+  print(--a);
+
+
+  print(a++);
+  print(a++);
+  print(a--);
+  print(a--);
+
+  print("___________________");
+
+  const thisYear = 2025;
+  print(thisYear);
+
+
+}
---
+++ test/dart_pratice_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_pratice/dart_pratice.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
