cd45085a1c73985d416dc5f2ceee4f6b7a1d8575
MD.Kamrul Hasan ⚡️ <kamrulhasan635653@gmail.com>
2025-06-03 00:48:51 +0600

    Added Map, list, Set

--- bin/dart_pratice.dart
+++ bin/dart_pratice.dart
@@ -55,5 +55,64 @@ void main() {
   const thisYear = 2025;
   print(thisYear);
 
+  final currentYear = 2025;
+  print(currentYear);
+
+  var result = city.reversed;
+  print(result);
+  var resultIsEmpt = city.isEmpty;
+  print(resultIsEmpt);
+  print(city.isNotEmpty);
+  // print(city.single);
+
+
+  print(city);
+  city.add("CoxBazar");
+  city.add("Khulna");
+  city.insert(0, "Sandwip");
+  city.addAll([1,2,3,4,5,6]);
+  city.insertAll(4, [9,8,7,6]);
+  city[0] = "Sandwip Chattogram";
+  city.removeAt(10);
+  city.remove(5);
+  print(city);
+
+
+//   SET
+
+var countries = <String>{"Bangladesh", "India", "Pakistan", "China"};
+countries.add("Nepal");
+countries.addAll({"USA", "Dhaka"});
+var d = countries.elementAt(5);
+print(d);
+// countries.clear();
+print(countries);
+
+
+
+// Map
+
+var person = {
+  'name': "Tom",
+  'age': 23,
+  'city': 'Dhaka'
+};
+
+person['country'] = "Bangladesh";
+print(person);
+
+
+  var thisIsMap = new Map();
+
+  thisIsMap['name'] = 'MD.Kamrul Hasan';
+  thisIsMap['age'] = 27;
+
+  print(thisIsMap);
+  print(thisIsMap.keys);
+  print(thisIsMap.values);
+  print(thisIsMap.length);
+  print(thisIsMap.isNotEmpty);
+  print(thisIsMap.isEmpty);
+
 
 }
